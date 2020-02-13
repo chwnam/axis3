bc9d3e856d6e42bb3747429ab3f1f708872462fc
changwoo <ep6tri@hotmail.com>
2020-02-13 16:56:17 +0900

    DatePickerWidget 에서 시간도 입력받을 수 있도록 수정

--- src/Views/Admin/FieldWidgets/DatePickerWidget.php
+++ src/Views/Admin/FieldWidgets/DatePickerWidget.php
@@ -3,11 +3,13 @@
 namespace Shoplic\Axis3\Views\Admin\FieldWidgets;
 
 use DateTimeZone;
+
 use function Shoplic\Axis3\Functions\closeTag;
 use function Shoplic\Axis3\Functions\datetimeI18n;
 use function Shoplic\Axis3\Functions\getTimezone;
 use function Shoplic\Axis3\Functions\inputTag;
 use function Shoplic\Axis3\Functions\openTag;
+use function Shoplic\Axis3\Functions\strStartsWith;
 
 /**
  * Class DatePickerWidget
@@ -17,6 +19,7 @@ use function Shoplic\Axis3\Functions\openTag;
  *
  * @link    https://api.jqueryui.com/datepicker/
  * @link    https://jqueryui.com/datepicker/
+ * @link    https://trentrichardson.com/examples/timepicker/
  * @link    https://developer.wordpress.org/reference/functions/wp_enqueue_script/
  */
 class DatePickerWidget extends BaseFieldWidget
@@ -27,6 +30,18 @@ class DatePickerWidget extends BaseFieldWidget
 
         if (!$this->args['dateFormat']) {
             $this->args['dateFormat'] = $this->getFieldModel()->getValueType()->getArg('dateFormat') ?? 'Y-m-d';
+        }
+
+        // DATE_ATOM 은 DatetimeType 의 기본값이다. 기본값대로 안 하면 'dateFormat', 'timeFormat', 'datepickerOpt'
+        // 세 속성에 유의해서 세팅해야 한다.
+        if (DATE_ATOM === $this->args['dateFormat']) {
+            $this->args['dateFormat'] = substr(DATE_ATOM, 0, 5);
+            if ($this->args['timepickerAddon'] && !$this->args['timeFormat']) {
+                $this->args['timeFormat'] = substr(DATE_ATOM, 5);
+                if (strStartsWith($this->args['timeFormat'], '\\T')) {
+                    $this->args['timeFormat'] = str_replace('\\T', '\'T\'', $this->args['timeFormat']);
+                }
+            }
         }
 
         if (is_string($this->args['timezone'])) {
@@ -44,35 +59,60 @@ class DatePickerWidget extends BaseFieldWidget
          * @link https://www.php.net/manual/en/datetime.createfromformat.php
          * @link https://api.jqueryui.com/datepicker/#utility-formatDate
          */
-        $datepickerDateFormat = str_replace(
-            [
-                'j', // 날짜 앞에 0 붙이지 않게.
-                'd', // 날짜 앞에 0 붙여서.
-                'D', // 요일 표현 짧게. (Mon, Tue, ...)
-                'l', // 요일 표현 길게. (Monday, Tuesday, ...)
-                'z', // 0 - 365 사이 날짜. (0부터 시작)
-                'M', // 월을 문자로 짧게. (Jan, Feb, ...)
-                'F', // 월을 문자로 길게. (January, February, ...)
-                'n', // 월을 숫자로 0 붙이지 않게.
-                'm', // 월을 숫자로 0을 붙여서.
-                'y', // 년도를 짧게. 뒤 두 자리만.
-                'Y', // 년도를 길게. 4자리 모두.
-            ],
-            [
-                'd',
-                'dd',
-                'D',
-                'DD',
-                'o',
-                'M',
-                'MM',
-                'm',
-                'mm',
-                'y',
-                'yy',
-            ],
-            $this->args['dateFormat']
-        );
+        $search = [
+            'j', // 날짜 앞에 0 붙이지 않게.
+            'd', // 날짜 앞에 0 붙여서.
+            'D', // 요일 표현 짧게. (Mon, Tue, ...)
+            'l', // 요일 표현 길게. (Monday, Tuesday, ...)
+            'z', // 0 - 365 사이 날짜. (0부터 시작)
+            'M', // 월을 문자로 짧게. (Jan, Feb, ...)
+            'F', // 월을 문자로 길게. (January, February, ...)
+            'n', // 월을 숫자로 0 붙이지 않게.
+            'm', // 월을 숫자로 0을 붙여서.
+            'y', // 년도를 짧게. 뒤 두 자리만.
+            'Y', // 년도를 길게. 4자리 모두.
+            'G', // 24시간제 시간. 앞에 0 붙이지 않고.
+            'H', // 24시간제 시간. 앞에 0을 붙이고.
+            'g', // 12시간제 시간. 앞에 0을 붙이지 않고.
+            'h', // 12시간제 시간. 앞에 0을 붙이고.
+            'i', // 앞에 0을 붙인 분.
+            's', // 앞에 0을 붙인 초.
+            'v', // 앞에 0을 붙인 밀리초.
+            'u', // 앞에 0을 붙인 마이크로초.
+            'a', // am, pm (소문자)로 표시한 오전 오후.
+            'A', // AM, PM (대문자)로 표시한 오전 오후.
+            'e', // 시간대.
+            'P', // ISO 8601 포맷의 시간대 표시 (+04:45)
+        ];
+
+        $replace       = [
+            'd',
+            'dd',
+            'D',
+            'DD',
+            'o',
+            'M',
+            'MM',
+            'm',
+            'mm',
+            'y',
+            'yy',
+            'H',
+            'HH',
+            'h',
+            'hh',
+            'mm',
+            'ss',
+            'l',
+            'c',
+            'tt',
+            'TT',
+            'z',
+            'Z',
+        ];
+        $dateFormat    = str_replace($search, $replace, $this->args['dateFormat']);
+        $altTimeFormat = str_replace($search, $replace, $this->args['timeFormat']);
+        $timeFormat    = str_replace($search, $replace, get_option('time_format'));
 
         // picker field
         inputTag(
@@ -80,10 +120,10 @@ class DatePickerWidget extends BaseFieldWidget
                 $this->args['attrs'],
                 [
                     'id'       => $this->args['labelFor'],
-                    'value'    => datetimeI18n($this->getValue(), 'date'),
+                    'value'    => datetimeI18n($this->getValue(), $this->args['timepickerAddon'] ? 'both' : 'date'),
                     'type'     => 'text',
                     'class'    => 'text axis3-field-widget axis3-datepicker-widget',
-                    'style'    => 'cursor: pointer;',
+                    'style'    => 'cursor: pointer; ',
                     'required' => $this->isRequired(),
                     'title'    => $this->getRequiredMessage(),
                 ]
@@ -96,7 +136,10 @@ class DatePickerWidget extends BaseFieldWidget
             [
                 'id'    => $this->getId(),
                 'name'  => $this->getName(),
-                'value' => datetimeI18n($this->getValue(), $this->args['dateFormat']),
+                'value' => datetimeI18n(
+                    $this->getValue(),
+                    $this->getFieldModel()->getValueType()->getArg('dateFormat')
+                ),
                 'class' => 'axis3-field-widget axis3-datepicker-widget hidden',
                 'type'  => 'hidden',
             ]
@@ -118,8 +161,12 @@ class DatePickerWidget extends BaseFieldWidget
             wp_parse_args(
                 $this->args['datepickerOpt'] ?? [],
                 [
-                    'altField'  => '#' . $this->getId(),
-                    'altFormat' => $datepickerDateFormat,
+                    'timeFormat'       => $timeFormat,
+                    'altField'         => '#' . $this->getId(),
+                    'altFormat'        => $dateFormat,
+                    'altSeparator'     => strStartsWith($altTimeFormat, '\'T\'') ? '' : ' ',
+                    'altTimeFormat'    => $altTimeFormat,
+                    'altFieldTimeOnly' => false,
                 ]
             )
         );
@@ -128,9 +175,11 @@ class DatePickerWidget extends BaseFieldWidget
             $opt = '{}';
         }
 
+        $addon = $this->args['timepickerAddon'] ? 'true' : 'false';
+
         wp_add_inline_script(
             'axis3-datepicker-widget',
-            "jQuery('#{$this->getId()}-picker').axis3Datepicker({$opt});"
+            "jQuery(function(\$){\$('#{$this->getId()}-picker').axis3Datepicker({$opt}, {$addon});});"
         );
     }
 
@@ -142,11 +191,30 @@ class DatePickerWidget extends BaseFieldWidget
     {
         if (!wp_style_is('axis3-jquery-ui')) {
             wp_enqueue_style('axis3-jquery-ui');
+            if ($this->args['timepickerAddon'] && !wp_style_is('axis3-timepicker-addon')) {
+                wp_enqueue_style('axis3-timepicker-addon');
+            }
         }
 
         if (!wp_script_is('axis3-datepicker-widget')) {
             wp_enqueue_script('axis3-datepicker-widget');
             wp_localize_jquery_ui_datepicker();
+            if ($this->args['timepickerAddon'] && !wp_script_is('axis3-timepicker-addon')) {
+                wp_enqueue_script('axis3-timepicker-addon');
+                wp_enqueue_script('axis3-timepicker-addon-i18n');
+                $locale = $this->args['localize'];
+                if (!$locale) {
+                    $locale = get_option('WPLANG', 'en');
+                    if (false !== strpos($locale, '_')) {
+                        $locale = explode('_', $locale);
+                        $locale = $locale[0];
+                    }
+                }
+                wp_add_inline_script(
+                    'axis3-datepicker-widget',
+                    "jQuery(document).ready(function(\$){\$.timepicker.setDefaults('{$locale}');});"
+                );
+            }
         }
     }
 
@@ -159,23 +227,46 @@ class DatePickerWidget extends BaseFieldWidget
                  * array: 키-값 쌍으로 된 속성 목록.
                  *        피커가 나오는 input 태그에 사용된다. 실제 값을 가지는 hidden 에는 적용되지 않는다.
                  */
-                'attrs'         => [],
+                'attrs'           => [],
 
                 /**
-                 * null|string: 문자열을 해석할 포맷. 지정하지 않으면 값 타입에서 가져온다.
+                 * null|string: 날짜 형식 문자열을 해석할 포맷. 지정하지 않으면 값 타입에서 가져온다.
                  *              만약 값 타입에 'dateFormat' 인자가 지정되어 있지 않다면 Y-m-d 형식을 쓴다.
                  */
-                'dateFormat'    => null,
+                'dateFormat'      => null,
+
+                /**
+                 * null|string: 시간 형식 문자열 해석할 포맷. 지정하지 않으면 값 타입에서 가져온다.
+                 *              만약 값 타입에 'dateFormat' 인자가 지정되어 있지 않다면 H-i-s 형식을 쓴다.
+                 */
+                'timeFormat'      => null,
 
                 /**
                  * null|string: 시간대. 지정하지 않으면 값 타입에서 가져온다.
+                 *
                  */
-                'timezone'      => null,
+                'timezone'        => null,
 
                 /**
-                 * datepicker 로 전달할 초기화 객체.
+                 * array: datepicker 로 전달할 초기화 객체.
                  */
-                'datepickerOpt' => [],
+                'datepickerOpt'   => [],
+
+                /**
+                 * bool: timepicker addon 위젯을 추가로 붙인다.
+                 *       true 인 경우 timepicker addon 이 추가로 생긴다.
+                 *       모델 필드가 DatetimeType 이고 기본 dateFormat 을 쓰면 위젯에서 적절히 처리한다.
+                 *
+                 *       원래 datepicker 는 날짜만 취급하고 시간을 취급하지 않는다.
+                 *       그래서 timepicker addon 은 추가로 시간의 형식만 따로 설정하는 필드를 가지고 있다.
+                 *       위젯에서 시간 형식이 올바로 적용되려면 timeFormat 인자에 주의를 기울여야 한다.
+                 */
+                'timepickerAddon' => false,
+
+                /**
+                 * string: 로컬라이즈. 놔두면 현재 언어에 맞춘다.
+                 */
+                'localize'        => null,
             ]
         );
     }
--- src/assets/js/admin/field-widgets/datepicker.js
+++ src/assets/js/admin/field-widgets/datepicker.js
@@ -1,19 +1,26 @@
 if (window.hasOwnProperty('jQuery') && 'function' === typeof window.jQuery) {
     (function ($) {
-        $.fn.axis3Datepicker = function (opt) {
+        $.fn.axis3Datepicker = function (opt, addon) {
             var $this = this;
-            if ('function' === typeof $.fn.datepicker) {
+
+            if (addon) {
+                $.timepicker.setDefaults($.timepicker.regional['ko']);
+                $this
+                    .datetimepicker(opt || {})
+                    .on('keypress', function (e) {
+                        e.preventDefault();
+                    });
+            } else {
                 $this
                     .datepicker(opt || {})
                     .on('keypress', function (e) {
                         e.preventDefault();
                     });
-
-                $this.siblings('.axis3-datepicker-widget.button').click(function () {
-                    $this.siblings('.axis3-datepicker-widget.hidden').val('');
-                    $this.val('');
-                });
             }
+            $this.siblings('.axis3-datepicker-widget.button').click(function () {
+                $this.siblings('.axis3-datepicker-widget.hidden').val('');
+                $this.val('');
+            });
             return this;
         }
     })(window.jQuery);
