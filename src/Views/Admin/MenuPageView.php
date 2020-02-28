9a2c23dcaf5ff281b06d09cb2c96f851aabcfe3f
changwoo <ep6tri@hotmail.com>
2020-02-28 22:25:16 +0900

    fixAdminHook 추가
    커스텀 테이블 사용시 꽤 중요한 역할을 한다. 주석 참고.

--- src/Views/Admin/MenuPageView.php
+++ src/Views/Admin/MenuPageView.php
@@ -17,6 +17,12 @@ abstract class MenuPageView extends BaseView
     /** @var string */
     protected $hook = '';
 
+    /**
+     * @var bool
+     * @see alterAdminPageHook()
+     */
+    protected $fixAdminHook = false;
+
     abstract public static function getPageTitle(): string;
 
     abstract public static function getMenuTitle(): string;
@@ -26,6 +32,11 @@ abstract class MenuPageView extends BaseView
     abstract public static function getMenuSlug(): string;
 
     abstract public function dispatch();
+
+    public function setup($args = array())
+    {
+        $this->fixAdminHook = boolval($args['fixAdminHook'] ?? false);
+    }
 
     public function getCallback()
     {
@@ -49,6 +60,10 @@ abstract class MenuPageView extends BaseView
 
     public function addMenuPage()
     {
+        if ($this->fixAdminHook) {
+            add_filter('sanitize_title', [$this, 'alterAdminPageHook'], 10, 3);
+        }
+
         $this->hook = add_menu_page(
             static::getPageTitle(),
             static::getMenuTitle(),
@@ -58,5 +73,42 @@ abstract class MenuPageView extends BaseView
             $this->getIconUrl(),
             $this->getPosition()
         );
+
+        if ($this->fixAdminHook) {
+            remove_filter('sanitize_title', [$this, 'alterAdminPageHook']);
+        }
+    }
+
+    /**
+     * 페이지 훅 이름을 교정
+     *
+     * add_menu_page() 내부에 있는 sanitize_title() 함수에서
+     * 메뉴 이름이 URLEncode 처리되어 $admin_page_hooks 라는 글로별 변수에 기록된다.
+     *
+     * 코어의 메뉴 이름들은 다 영소문자를 써서 관계 없을지 모르지만,
+     * 이렇게 인코딩 처리된 이름은 리스트 테이블의 열 숨김 AJAX 요청인 hidden-columns 액션 콜백 함수 내부에서
+     * sanitize_key() 함수 통과를 할 수가 없어 동작이 멈추는 버그가 발생한다.
+     * 어차피 큰 버그가 없다면 AJAX 콜백이 제대로 동작하도록 이름을 교정하면 된다.
+     *
+     * @callback
+     * @filter      sanitize_title
+     *
+     * @param string $title    페이지 훅 이름
+     * @param string $rawTitle 원래 그대로의 메뉴 이름.
+     * @param string $context  보통 'save'로 입력됨.
+     *
+     * @return string
+     *
+     * @see         add_menu_page()
+     * @see         sanitize_title()
+     * @see         wp_ajax_hidden_columns()
+     * @see         $admin_page_hooks
+     */
+    public function alterAdminPageHook($title, $rawTitle, $context)
+    {
+        if ($rawTitle === static::getMenuTitle() && 'save' === $context) {
+            $title = static::getMenuSlug();
+        }
+        return $title;
     }
 }
