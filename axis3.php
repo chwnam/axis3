4f79eac54179aa8c1f861fc898b65b633b059a55
changwoo <ep6tri@hotmail.com>
2020-03-15 11:27:37 +0900

    버전, 체인지로그 수정

--- axis3.php
+++ axis3.php
@@ -17,7 +17,7 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-define('AXIS3_VERSION', '0.1.4');
+define('AXIS3_VERSION', '0.1.5');
 define('AXIS3_MAIN', __FILE__);
 
 if (defined('WP_CLI') && WP_CLI && class_exists('WP_CLI')) {
@@ -30,4 +30,3 @@ if (defined('WP_CLI') && WP_CLI && class_exists('WP_CLI')) {
 
 add_action( 'admin_enqueue_scripts', 'Shoplic\Axis3\Functions\adminEnqueueScripts' );
 
-// TODO: 다양한 값 타입 흡수. 자버의 좌표, 맵
